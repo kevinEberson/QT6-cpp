164bf07d36b250252421b0aac67b83c087d111bf
Kevin <459792@fhict.nl>
2024-02-29 10:07:27 +0100

    poc communication between multiple threads

---
+++ proof_of_concepts/qt_ep22_Threading_communication/main.cpp
@@ -0,0 +1,35 @@
+#include <QCoreApplication>
+#include <QThread>
+#include "worker.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QThread pThread;
+    QThread cThread;
+
+    Worker producer;
+    Worker consumer;
+
+    producer.moveToThread(&pThread);
+    consumer.moveToThread(&cThread);
+
+    producer.setProducer(true);
+
+    pThread.setObjectName("Producer thread");
+    cThread.setObjectName("Consumer thread");
+    producer.setObjectName("Producer");
+    consumer.setObjectName("Consumer");
+
+    QObject::connect(&pThread, &QThread::started, &producer, &Worker::start);
+    QObject::connect(&cThread, &QThread::started, &consumer, &Worker::start);
+
+    QObject::connect(&producer, &Worker::produced, &consumer, &Worker::consume);
+    QObject::connect(&producer, &Worker::finished, &consumer, &Worker::quit);
+
+    cThread.start();
+    pThread.start();
+
+    return a.exec();
+}
